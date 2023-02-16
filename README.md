fab75ad4ef0b87fea5ddac744d46e213b25dde2b
Charles Baguley <charlesbaguley@gmail.com>
2023-02-16 10:29:04 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Node.js-Express---MyLearning
+
+Back End Development and APIs course found on freecodecamp. 
+Covered topics include: NPM, Node.js, Express, MongoDB and Mongoose.
---
+++ myApp.js
@@ -0,0 +1,51 @@
+let express = require('express');
+let app = express();
+let absolutePath = __dirname + "/views/index.html";
+
+
+
+  app.use(express.static(__dirname + "/public"));
+
+  app.get("/", function(req, res) {
+    res.sendFile(absolutePath);
+  });
+
+
+console.log("Hello World");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
